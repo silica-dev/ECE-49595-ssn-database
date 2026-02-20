8b6d6a89ef4aae8b957ae2243bc1282b0969a454
silicasandwhich <silicasandwhich@github.com>
2026-02-20 00:04:13 -0500

    organize folders again

--- clear_database.py
+++
@@ -1,9 +0,0 @@
-from sqlite3 import Connection
-
-con = Connection("users.db")
-cur = con.cursor()
-
-cur.execute("DELETE FROM users;")
-cur.close()
-con.commit()
-con.close()
--- create_token_db.py
+++
@@ -1,9 +0,0 @@
-from sqlite3 import Connection
-
-con = Connection("tokens.db")
-cur = con.cursor()
-
-cur.execute("CREATE TABLE revoked_tokens (jti WORD)")
-
-con.commit()
-con.close()
